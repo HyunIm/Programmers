3523551209e8d2b7ebf44c5d16a69263e772567e
Lim Hyun <hyunzion@gmail.com>
2021-09-11 19:06:12 +0900

    Mod Weekly Challenge Problem

--- Problem/83201.py
+++ Problem/83201.py
@@ -1,4 +1,4 @@
-# File : 823201.py
+# File : 83201.py
 # Dev : LimHyun (hyunzion@gmail.com)
 # Since : 2021-09-11
 # Brief : 위클리 챌린지
@@ -6,39 +6,16 @@
 def solution(scores):
     grade = []
     length = len(scores)
+    a = list(map(list, zip(*scores)))
     answer = ''
 
-    for i in range(length):
-        s = 0
-        s_max = 0
-        s_min = 100
-
-        for j in range(length):
-            s += scores[j][i]
-            s_max = s_max if s_max >= scores[j][i] else scores[j][i]
-            s_min = s_min if s_min <= scores[j][i] else scores[j][i]
-
-        if s_max == scores[i][i]:
-            f = 0
-            for j in range(length):
-                if scores[j][i] == s_max:
-                    f += 1
-            if f == 1:
-                s -= scores[i][i]
-                s //= (length - 1)
-            else: s //= length
-
-        elif s_min == scores[i][i]:
-            f = 0
-            for j in range(length):
-                if scores[j][i] == s_min:
-                    f += 1
-            if f == 1:
-                s -= scores[i][i]
-                s //= (length - 1)
-            else: s //= length
-
-        else: s //= length
+    for i, j in enumerate(a):
+        s = sum(j)
+        if (max(j) == a[i][i] and j.count(max(j)) == 1) or (min(j) == a[i][i] and j.count(min(j)) == 1):
+            s -= a[i][i]
+            s //= (length - 1)
+        else:
+            s //= length
         grade.append(s)
 
     for i in grade:
